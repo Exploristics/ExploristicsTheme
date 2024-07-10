a60e990cc72aaf9e13393238a65fb77fa774d550
Gareth Burns <gburns12@qub.ac.uk>
2024-07-10 13:57:06 +0100

    chore(ExecuteOnAttach): Creating separate testable function

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,9 +1,5 @@
 # On loading package set the theme as Exploristics Theme
 
 .onAttach <- function(libname, pkgname) {
-  theme_set(exploristics_theme())
-  options(ggplot2.discrete.colour = ExploristicsTheme::Expl_Rebrand,
-          ggplot2.discrete.fill = ExploristicsTheme::Expl_Rebrand,
-          ggplot2.continuous.colour = scale_colour_exploristics,
-          ggplot2.continuous.fill = scale_fill_exploristics)
+  ExecuteOnAttach()
 }

556c4c3fba6638376b5063669543ae6c0774444f
Gareth Burns <gburns12@qub.ac.uk>
2024-07-29 15:19:01 +0100

    chore():Removing hased out code

--- R/onAttach.R
+++ R/onAttach.R
@@ -3,8 +3,6 @@
 .onAttach <- function(pkgname, libname) {
   theme_set(exploristics_theme())
 
-  # grDevices::colorRampPalette(get("Expl_Rebrand"))(9)
-
   options(ggplot2.discrete.colour = ExploristicsTheme::Expl_Rebrand,
           ggplot2.discrete.fill = ExploristicsTheme::Expl_Rebrand,
           ggplot2.continuous.colour = scale_colour_exploristics,

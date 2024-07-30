38a84e23efd61a218e1ab640025d6954f8d930ae
Gareth Burns <gburns12@qub.ac.uk>
2024-07-30 08:10:11 +0100

    style(): Applying R studio styler

--- R/onAttach.R
+++ R/onAttach.R
@@ -3,8 +3,10 @@
 .onAttach <- function(pkgname, libname) {
   theme_set(exploristics_theme())
 
-  options(ggplot2.discrete.colour = ExploristicsTheme::Expl_Brand[c(1,7,4,2,5,3, 6)],
-          ggplot2.discrete.fill = ExploristicsTheme::Expl_Brand[c(1,7,4,2,5,3, 6)],
-          ggplot2.continuous.colour = scale_colour_exploristics,
-          ggplot2.continuous.fill = scale_fill_exploristics)
+  options(
+    ggplot2.discrete.colour = ExploristicsTheme::Expl_Brand[c(1, 7, 4, 2, 5, 3, 6)],
+    ggplot2.discrete.fill = ExploristicsTheme::Expl_Brand[c(1, 7, 4, 2, 5, 3, 6)],
+    ggplot2.continuous.colour = scale_colour_exploristics,
+    ggplot2.continuous.fill = scale_fill_exploristics
+  )
 }

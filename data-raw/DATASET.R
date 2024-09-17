4f3481040ebc5ff25fccbfb8a9f3d6c89059fb31
Gareth Burns <gburns12@qub.ac.uk>
2024-09-17 14:48:39 +0100

    data(): Updating Package Data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,8 +2,20 @@
 
 # colour palettes
 Expl_Brand <- c(
-  "#18213B", "#718BDE", "#A4B8DC", "#89D8C1", "#DEF58C", "#EFC3B1", "#CD8581"
+  "#121a31",
+  "#2d3750",
+  "#cfdbef",
+  "#6ddabf",
+  "#4c9d88",
+  "#ae6867",
+  "#d8807f",
+  "#ca9e8e",
+  "#f6c0ad",
+  "#b1ca65",
+  "#d7f67c",
+  "#576fb8"
 )
+
 Expl_Blue <-
   c("#002659",
     "#293D94",
@@ -26,10 +38,12 @@ Expl_HighCont <-
     "#BF00B5",
     "#5EB17D")
 
-GradientLUT <- c(Expl_Brand = c(1,2),
-                 Expl_Blue = seq(length(Expl_Blue)),
-                 Expl_External = c(1, 6, 2),
-                 Expl_HighCont = c(1, 2, 4))
+GradientLUT <- c(
+  Expl_Brand = c(1, 2),
+  Expl_Blue = seq(length(Expl_Blue)),
+  Expl_External = c(1, 6, 2),
+  Expl_HighCont = c(1, 2, 4)
+)
 
 Text_Colour <- Expl_Brand[1]
 Footer_Text_Colour <- Expl_Brand[7]
